ec273bb576e37fae3c49a7acde7a9f7e1d7160da
ltndat <ltndat@outlook.com>
2022-09-17 15:00:55 +0700

    09/17/2022 15:00:55

--- src/pwsh/runtime.ps1
+++ src/pwsh/runtime.ps1
@@ -4,7 +4,7 @@ if ($IsWindows) {
   scoop install git wget
   scoop bucket add main
   scoop bucket add extras
-  scoop install msys2 sudo nodejs python vscode neovim
+  scoop install windows-terminal msys2 sudo nodejs python vscode neovim
 } else {
   foreach ($i in $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)) {iex $i}  
   brew install git wget python node neovim
