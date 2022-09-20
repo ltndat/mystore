4413cd18b31ff1ff6f361a73443e3449949ca929
ltndat <ltndat@outlook.com>
2022-09-20 16:33:50 +0700

    09/20/2022 16:33:50

--- src/shell/setup_win32.bat
+++ src/shell/setup_win32.bat
@@ -7,5 +7,4 @@ scoop bucket add main
 scoop install sudo
 
 @REM Chocolatety
-sudo powershell -c ""Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')); foreach ($i in $(curl.exe -s https://raw.githubusercontent.com/ltndat/mystore/main/src/choco_listapps_dev.txt)) {choco install $i -y};""
-echo '123'
+sudo powershell -c "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;irm https://community.chocolatey.org/install.ps1 | iex; foreach ($i in $(curl.exe -s https://raw.githubusercontent.com/ltndat/mystore/main/src/choco_listapps.txt)) {choco install $i -y};"
