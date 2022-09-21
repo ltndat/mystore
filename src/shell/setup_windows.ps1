2a0617a368d8f7a8428ebe795d0d9ae949091cd5
ltndat <ltndat@outlook.com>
2022-09-21 10:49:04 +0700

    09/21/2022 10:49:03

--- src/shell/setup_windows.ps1
+++ src/shell/setup_windows.ps1
@@ -2,9 +2,9 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 
-scoop install git
-scoop bucket add main
-scoop install sudo wget
+# scoop install git
+# scoop bucket add main
+# scoop install sudo wget
 
 # Chocolatety
 Set-ExecutionPolicy Bypass -Scope Process -Force
