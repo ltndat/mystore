042f965533545bbe9cf86971c546bd3effc2e196
ltndat <ltndat@outlook.com>
2022-09-21 10:47:50 +0700

    09/21/2022 10:47:50

--- src/shell/setup_myflow.ps1
+++ src/shell/setup_myflow.ps1
@@ -1,12 +1,15 @@
 $IsWsl = ($env:WSL_DISTRO_NAME -ne $null)
 
 if ($IsWindows) {
+  git clone https://github.com/ltndat/myshell.git $env:USERPROFILE/.config/myshell
+  git clone https://github.com/ltndat/myapps.git $env:USERPROFILE/.config/myapps
+  git clone https://github.com/ltndat/mystore.git $env:USERPROFILE/.config/mystore
   scoop bucket add main
   scoop bucket add extras
   scoop install wget msys2 sudo nodejs python vscode neovim
 } else {
   foreach ($i in $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)) {iex $i}  
-  brew install git wget python node neovim
+  brew install python node neovim
   if ($IsWsl -eq $false) { brew install --cask visual-studio-code }
   # set alias
   sudo ln -sf (which python3) /usr/local/bin/python
--- src/shell/setup_unix.sh
+++ src/shell/setup_unix.sh
@@ -1,1 +1,7 @@
-echo 'hello bash'
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if [[ -z "${DEPLOY_ENV}" ]]; then
+  MY_SCRIPT_VARIABLE="Some default value because DEPLOY_ENV is undefined"
+else
+  MY_SCRIPT_VARIABLE="${DEPLOY_ENV}"
+fi
