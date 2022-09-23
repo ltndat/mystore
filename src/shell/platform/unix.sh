03ab241a0f5f58744de9cc17855210da58e9c6a3
ltndat <ltndat@outlook.com>
2022-09-23 10:33:29 +0700

    09/23/2022 10:33:29

--- src/shell/platform/unix.sh
+++ src/shell/platform/unix.sh
@@ -43,6 +43,6 @@ test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
-brew install git wget
+# brew install git wget
 
 echo 'Done!'
