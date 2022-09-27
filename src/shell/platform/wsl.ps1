a60d024468975eb44459e0ebc34fe479c51c6b57
ltndat <ltndat@outlook.com>
2022-09-27 15:28:18 +0700

    09/27/2022 15:28:17

--- src/shell/platform/wsl.ps1
+++ src/shell/platform/wsl.ps1
@@ -27,10 +27,11 @@ wsl --set-default $distro
 # rm ./wsl_update_x64.msi
 
 # add default user
-start wsl -Wait 'read -p "New username: " username; sudo echo "" >> /etc/wsl.conf; sudo echo "[user]" >> /etc/wsl.conf; sudo echo "default=$username" >> /etc/wsl.conf; sudo echo "$username ALL=(ALL) ALL" >> /etc/sudoers; sudo useradd -m $username; sudo passwd $username; ;exit' 
+start wsl -Wait 'read -p "New username: " username; sudo echo "" >> /etc/wsl.conf; sudo echo "[user]" >> /etc/wsl.conf; sudo echo "default=$username" >> /etc/wsl.conf; sudo echo "$username ALL=(ALL) ALL" >> /etc/sudoers; sudo useradd -m $username; sudo passwd $username;' 
 wsl --shutdown
 
-start wsl -Wait 'sudo pacman-key --init; sudo pacman-key --populate archlinux; echo Y | sudo pacman -S archlinux-keyring; echo Y | sudo pacman -Su; exit'
+start wsl -Wait 'sudo pacman-key --init; sudo pacman-key --populate archlinux;'
+start wsl -Wait 'echo Y | sudo pacman -S archlinux-keyring; echo Y | sudo pacman -Su;'
 wsl --shutdown 
 
 start wsl -Wait 'curl -fsSL https://raw.githubusercontent.com/ltndat/mystore/main/src/shell/platform/unix.sh > /tmp/setup.sh; source /tmp/setup.sh; rm /tmp/setup.sh'
